00ca23b032effc2300c51dbfc68e27188c015bd3
azharcse14 <mdazharcse14@gmail.com>
2021-09-09 20:41:53 +0600

    Create home page and add new activity

---
+++ app/src/main/java/com/azhar/pixeleditor/View/EditImageActivity.kt
@@ -0,0 +1,12 @@
+package com.azhar.pixeleditor.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azhar.pixeleditor.R
+
+class EditImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_image)
+    }
+}
--- app/src/main/java/com/azhar/pixeleditor/View/MainActivity.kt
+++ app/src/main/java/com/azhar/pixeleditor/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.azhar.pixeleditor
+package com.azhar.pixeleditor.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.azhar.pixeleditor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
