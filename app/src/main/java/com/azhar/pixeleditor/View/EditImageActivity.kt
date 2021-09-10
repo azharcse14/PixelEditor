455c919a3ee30f8815bd05b15de08c66588e38d8
azharcse14 <mdazharcse14@gmail.com>
2021-09-10 10:07:03 +0600

    Image import and display

--- app/src/main/java/com/azhar/pixeleditor/View/EditImageActivity.kt
+++ app/src/main/java/com/azhar/pixeleditor/View/EditImageActivity.kt
@@ -1,12 +1,39 @@
 package com.azhar.pixeleditor.View
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.azhar.pixeleditor.R
+import com.azhar.pixeleditor.databinding.ActivityCameraXactivityBinding.inflate
+import com.azhar.pixeleditor.databinding.ActivityEditImageBinding
+import com.azhar.pixeleditor.databinding.ActivityMainBinding
 
 class EditImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_image)
+        binding = ActivityEditImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        displayImagePreview()
+    }
+
+    private fun displayImagePreview() {
+        intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            binding.imagePreview.setImageBitmap(bitmap)
+            binding.imagePreview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setListeners() {
+        binding.imageBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
